ee66d4a81c88a8dbe653093fae306453193ed446
konishiakira <kurokan014sdgo22@gmail.com>
2021-10-06 21:31:15 +0900

    ユーザ管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :f_name, presence: true
+  validates :l_name, presence: true
+  validates :f_name_f, presence: true
+  validates :l_name_f, presence: true
+  validates :birthday, presence: true
+
+  # -------------
+  # validates :content, presence: true, unless: :was_attached?
+  # unless部分：メソッドの返り値がfalseならば
+  # バリデーションによる検証を行う
+
+  # def was_attached?
+  #   self.image.attached?
+  # レコードにファイルが添付されているかどうかでtrueかfalseを返す
+  # -------------
+
 end
