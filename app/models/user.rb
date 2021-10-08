858998e6e828fc914427c26b7d2450271947e785
konishiakira <kurokan014sdgo22@gmail.com>
2021-10-08 20:17:32 +0900

    モデル、ビューファイル修正及び新規登録できない場合のテストコード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,21 +14,20 @@ class User < ApplicationRecord
   # ニックネームが必須であること。OK
 
   validates :password,
-            length: { minimum: 6 },
-            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
-            confirmation: true
-  validates :password_confirmation, presence: true
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  #           confirmation: true
+  # validates :password_confirmation, presence: true
   # パスワードは、6文字以上での入力が必須であること OK
   # パスワードは、半角英数字混合での入力が必須であること OK
   # パスワードとパスワード（確認）は、値の一致が必須であること。 OK
 
   validates :f_name, presence: true,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/}
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   # お名前(全角)は、名字と名前がそれぞれ必須であること。OK
   # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること。OK
 
   validates :l_name, presence: true,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/}
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   # お名前(全角)は、名字と名前がそれぞれ必須であること。OK
   # お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること。OK
 
