3b402e62036c4a8efcc48f405d22074245e4eed6
konishiakira <kurokan014sdgo22@gmail.com>
2021-10-18 18:28:53 +0900

    コメントアウトとS3設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,44 +1,20 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :create,:edit]
   before_action :set_noedit, only: [:edit,:destroy]
-  # ログインしていなかったらリダイレクトでトップページに戻るよ！
-  # というのを、showアクションに反映させる
-  #購入機能追加時に追加
-  # before_action :souldout_to_index,only: [:edit,:update]
-
-  # 「比較のために記述」。下記はdeviseを用いない場合の記述。対象メソッドも同様にコメントアウト
-  # before_action :move_to_index, only: [:show]
 
   def index
     @items = Item.all.order("created_at DESC")
-
-    # @itemsends = Itemsend.all
-    # @itemsend = Itemsend.new
-    # @item = Item.find
-    # @itemsends = Itemsend.all
-    # @prototype = Prototype.find(params[:id])
-    # @comment = Comment.new
-    # @comments = @prototype.comments.includes(:user)
   end
+  
   def new
     @item = Item.new
-
-    # 「比較のために記述」
-    # @message = Message.new
-    # @room = Room.find(params[:room_id])
-    # @messages = @room.messages.includes(:user)
   end
 
   def show
     @items = Item.find(params[:id])
-    # 「比較のために記述」    
-    # @tweet = Tweet.find(params[:id])
-    # @comment = Comment.new
-    # @comments = @tweet.comments.includes(:user)
   end
 
   def edit
-    # @item = Item.find(params[:id])
     item_paramsid
     if @item.buylog != nil
       redirect_to action: :index
@@ -46,7 +22,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    # @item = Item.find(params[:id])
     item_paramsid
     if @item.update(item_params)
       redirect_to item_path(@item.id)
